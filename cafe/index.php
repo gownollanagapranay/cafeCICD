5a185423acb94fbd19dae819f7ab539c28a80bfb
gownollanagapranay <143612512+gownollanagapranay@users.noreply.github.com>
2025-04-12 12:54:24 +0530

    Update index.php

--- cafe/index.php
+++ cafe/index.php
@@ -34,7 +34,7 @@
 		<div id="mainPictures" class="center">
 			<table>
 				<tr>
-					<td><img src="images/indulgent-homemade-chocolate-chip-cookies-on-rustic-table-generated-by-ai-free-photo" height=auto width="490"></td>
+					<td><img src="images/indulgent-homemade-chocolate-chip-cookies-on-rustic-table-generated-by-ai-free-photo.jpg" height=auto width="490"></td>
 					<td><img src="images/Cake-Vitrine.jpg" height=auto width="450"></td>
 				</tr>
 			</table>
